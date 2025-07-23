a1e8586d77bd05c4a239396b6b78fff111fa7c7d
Nic Tjader <nictjader@gmail.com>
2025-07-23 20:29:22 +0000

    The logo on the homepage has an open book and the text. The logo as desi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,6 @@ export const metadata: Metadata = {
   title: 'FirstLook - Unforgettable first encounters.',
   description:
     'Discover captivating romance stories. Unlock premium content and immerse yourself in a world of passion and adventure.',
-  icons: {
-    icon: '/favicon.ico',
-  },
 };
 
 export default function RootLayout({
