8607d48aeb72aacb45eb94c5161770572454fa02
Nic Tjader <nictjader@gmail.com>
2025-07-30 22:18:01 +0000

    the icon and favicon should resemble the icon in the top left of the hom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,9 +28,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-      </head>
       <body
         className={`${playfairDisplay.variable} font-body`}
       >
