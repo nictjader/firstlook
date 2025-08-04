edeb5fa9621f33dbac564d2a1ccfb2d6303ec8af
Nic Tjader <nictjader@gmail.com>
2025-08-04 20:54:44 +0000

    check again

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
