aeaca1ed087e64cb06abef330424e58ed8ed668b
Nic Tjader <nictjader@gmail.com>
2025-08-06 03:09:41 +0000

    same

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -63,8 +63,8 @@ export default function Header() {
                   aria-label="Toggle theme"
                   className="text-primary hover:text-primary/80"
                 >
-                  <Sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                  <Moon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                  <Sun className="h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                  <Moon className="absolute h-5 w-5 rotate-0 scale-100 transition-all dark:rotate-90 dark:scale-0" />
                   <span className="sr-only">Toggle theme</span>
                 </Button>
               </TooltipTrigger>
