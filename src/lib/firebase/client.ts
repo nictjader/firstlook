e369370045bed84670bd25593cce213b64156669
Nic Tjader <nictjader@gmail.com>
2025-08-21 21:55:43 +0000

    i do NOT want a new databse or firebase project

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -7,12 +7,13 @@ import { getFunctions } from 'firebase/functions';
 
 // Get Firebase config from environment variables
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "REDACTED",
+  authDomain: "REDACTED",
+  projectId: "REDACTED",
+  storageBucket: "REDACTED",
+  messagingSenderId: "REDACTED",
+  appId: "REDACTED",
+  measurementId: "REDACTED"
 };
 
 // Validate that all required config values are present
