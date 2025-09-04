bce43c82d4c3945aff54d75a160ab8b88834ba73
Nic Tjader <nictjader@gmail.com>
2025-09-04 23:37:53 +0000

    progress? failed to build: (error ID: 5aada519): {"reason":"Failed Frame

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,10 @@ module.exports = {
   darkMode: ["class"],
   content: [
     './pages/**/*.{ts,tsx}',
-    'components/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-  ],
+	],
   prefix: "",
   theme: {
     container: {
