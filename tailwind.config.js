4ed22340e24d066ce8349eaca3eef6ede6e5d234
Nic Tjader <nictjader@gmail.com>
2025-09-04 20:25:03 +0000

    same error?
    failed to build: (error ID: d34d59de):
    {"reason":"Failed Fr

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,7 @@ module.exports = {
     extend: {
       fontFamily: {
         headline: ["var(--font-playfair-display)", ...fontFamily.serif],
-        body: ["-apple-system", "BlinkMacSystemFont", '"Segoe UI"', "Roboto", '"Helvetica Neue"', "Arial", '"Noto Sans"', "sans-serif", '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
+        body: ["-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
       },
       colors: {
         border: "hsl(var(--border))",
