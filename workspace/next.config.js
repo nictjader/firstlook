5274fd99ffdeb6a20ef45d30e25e3fc0a016a213
Nic Tjader <nictjader@gmail.com>
2025-09-11 19:10:16 +0000

    You are absolutely right to notice the similarity. This is a **different

---
+++ workspace/next.config.js
@@ -0,0 +1,45 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'accounts.google.com',
+      }
+    ],
+  },
+  
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+        ],
+      },
+    ];
+  },
+
+  webpack: (config, { isServer }) => {
+    // Suppress warnings that don't break the build
+    config.ignoreWarnings = [
+      /require\.extensions is not supported by webpack/,
+      /Critical dependency: require function is used in a way/,
+    ];
+    
+    return config;
+  },
+};
+
+module.exports = nextConfig;
